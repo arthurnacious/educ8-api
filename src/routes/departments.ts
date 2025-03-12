2e17ce13f2cad15d4e700558bfda6f56630f5172
Arthurnacious <arthurnacious@gmail.com>
2025-03-12 21:05:18 +0200

    feat: delete multiple departments route

--- src/routes/departments.ts
+++ src/routes/departments.ts
@@ -14,14 +14,18 @@ import { departmentUserRole } from "@/types/roles";
 import { slugify } from "@/utils";
 import { z } from "zod";
 
-const departmentSchema = z.object({
+const createDepartmentSchema = z.object({
   name: z
     .string()
     .min(2, "Name must be at least 2 characters")
     .max(100, "Name is too long"),
 });
 
-const attachUserToDepartmentSchema = z.object({
+const deleteDepartmentsSchema = z.object({
+  ids: z.array(z.string()),
+});
+
+const assignUserToDepartmentSchema = z.object({
   userId: z.string().min(1, {
     message: "User Must be selected",
   }),
@@ -29,6 +33,10 @@ const attachUserToDepartmentSchema = z.object({
     message: "Department Must be selected",
   }),
   role: z.nativeEnum(departmentUserRole).default(departmentUserRole.LECTURER),
+});
+
+const unassignUsersToDeprtmentSchema = z.object({
+  idObject: z.array(z.object({ userId: z.string(), departmentId: z.string() })),
 });
 
 const departments = new Hono<{ Variables: JwtVariables }>();
@@ -71,32 +79,35 @@ departments
 
     return ctx.json({ data });
   })
-  .get("/:slug", async (ctx) => {
-    const { slug } = ctx.req.param();
+  .patch("/", async (ctx) => {
+    try {
+      const body = await ctx.req.json(); // Get request body
+      const validatedData = deleteDepartmentsSchema.safeParse(body); // Validate input
+      console.log(validatedData.data);
+      if (!validatedData.success) {
+        return ctx.json({ error: validatedData.error.format() }, 400);
+      }
 
-    const data = await db.query.departmentsTable.findFirst({
-      where: (department, { eq }) => eq(department.slug, slug),
-      with: {
-        members: {
-          with: {
-            user: {
-              columns: {
-                id: true,
-                name: true,
-                email: true,
-              },
-            },
-          },
-        },
-      },
-    });
+      const { ids } = validatedData.data;
 
-    return ctx.json({ data });
+      const data = await Promise.all(
+        ids.map((departmentId) =>
+          db
+            .delete(departmentsTable)
+            .where(eq(departmentsTable.id, departmentId))
+        )
+      );
+
+      return ctx.json({ data }, 200);
+    } catch (error) {
+      console.error("Error deleting departments:", error);
+      return ctx.json({ error: "Internal Server Error" }, 500);
+    }
   })
   .post("/", async (ctx) => {
     try {
       const body = await ctx.req.json(); // Get request body
-      const validatedData = departmentSchema.safeParse(body); // Validate input
+      const validatedData = createDepartmentSchema.safeParse(body); // Validate input
 
       if (!validatedData.success) {
         return ctx.json({ error: validatedData.error.format() }, 400);
@@ -130,11 +141,33 @@ departments
       return ctx.json({ error: "Internal Server Error" }, 500);
     }
   })
+  .get("/:slug", async (ctx) => {
+    const { slug } = ctx.req.param();
+
+    const data = await db.query.departmentsTable.findFirst({
+      where: (department, { eq }) => eq(department.slug, slug),
+      with: {
+        members: {
+          with: {
+            user: {
+              columns: {
+                id: true,
+                name: true,
+                email: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return ctx.json({ data });
+  })
   .put("/:slug", async (ctx) => {
     const { slug } = ctx.req.param();
     try {
       const body = await ctx.req.json(); // Get request body
-      const validatedData = departmentSchema.safeParse(body); // Validate input
+      const validatedData = createDepartmentSchema.safeParse(body); // Validate input
 
       if (!validatedData.success) {
         return ctx.json({ error: validatedData.error.format() }, 400);
@@ -158,10 +191,10 @@ departments
       return ctx.json({ error: "Internal Server Error" }, 500);
     }
   })
-  .patch("/members", async (ctx) => {
+  .post("/members", async (ctx) => {
     try {
       const body = await ctx.req.json(); // Get request body
-      const validatedData = attachUserToDepartmentSchema.safeParse(body); // Validate input
+      const validatedData = assignUserToDepartmentSchema.safeParse(body); // Validate input
 
       if (!validatedData.success) {
         return ctx.json({ error: validatedData.error.format() }, 400);
@@ -174,6 +207,38 @@ departments
         departmentId,
         role,
       });
+
+      return ctx.json({ data }, 200);
+    } catch (error) {
+      console.error("Error attaching user to department:", error);
+      return ctx.json({ error: "Internal Server Error" }, 500);
+    }
+  })
+  .patch("/members", async (ctx) => {
+    try {
+      const body = await ctx.req.json();
+      const validatedData = unassignUsersToDeprtmentSchema.safeParse(body); // Validate input
+
+      if (!validatedData.success) {
+        return ctx.json({ error: validatedData.error.format() }, 400);
+      }
+
+      const { idObject } = validatedData.data;
+
+      console.log("idObject", idObject);
+
+      const data = await Promise.all(
+        idObject.map(({ userId, departmentId }) =>
+          db
+            .delete(userToDepartment)
+            .where(
+              and(
+                eq(userToDepartment.userId, userId),
+                eq(userToDepartment.departmentId, departmentId)
+              )
+            )
+        )
+      );
 
       return ctx.json({ data }, 200);
     } catch (error) {
